5de173fed7368f5a3d9455d69c17cc4b05e8ada2
Lewis Moore <lewis.moore2707@gmail.com>
2018-10-26 15:18:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
 
 model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' })
 
-const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4, ], [6, 1])
+const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1])
 const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1])
 
 model.fit(xs, ys, { epochs: 500 }).then(() => { 
