04f3593f1a8253ce9e9411897d87096cb91325c3
Lewis Moore <lewis.moore2707@gmail.com>
2018-10-26 15:32:26 +0100

    Update index.html with new ui. Update index.js to take user input as evaluation point

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import * as tf from '@tensorflow/tfjs'
 
-document.getElementById('output').innerText="Hello World"
-
 const model = tf.sequential()
 model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
 
@@ -12,5 +10,11 @@ const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1])
 const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1])
 
 model.fit(xs, ys, { epochs: 500 }).then(() => { 
-    model.predict(tf.tensor2d([5], [1, 1])).print()
+    document.getElementById('predictButton').disabled = false
+    document.getElementById('predictButton').innerText = "Predict"
+})
+
+document.getElementById('predictButton').addEventListener('click', (ele, ev) => {
+    let val = document.getElementById('inputValue').value 
+    document.getElementById('output').innerText = model.predict(tf.tensor2d([val], [1, 1]))
 })
