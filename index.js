b9805df8b89cbc4a115e7bc24cce9c7e0a496881
Lewis Moore <lewis.moore2707@gmail.com>
2018-10-26 13:53:41 +0100

    Add model to index

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import * as tf from '@tensorflow/tfjs'
 
 document.getElementById('output').innerText="Hello World"
+
+const model = tf.sequential()
+model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+
+model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' })
+
+const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4, ], [6, 1])
+const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1])
+
+model.fit(xs, ys, { epochs: 500 }).then(() => { 
+    model.predict(tf.tensor2d([5], [1, 1])).print()
+})
